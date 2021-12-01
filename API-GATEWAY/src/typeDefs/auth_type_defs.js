1627a0e207413c8d1ffeeaa0a32f0067c4ed03ce
Sergio Medina <sergiomsystem@gmail.com>
2021-12-01 08:58:52 -0500

    Add API-GATEWAY folder as well as add getSpecialistById and filterSpecialistByCategory

---
+++ API-GATEWAY/src/typeDefs/auth_type_defs.js
@@ -0,0 +1,61 @@
+const { gql } = require("apollo-server");
+
+const authTypeDefs = gql`
+  type Tokens {
+    refresh: String!
+    access: String!
+  }
+
+  type Access {
+    access: String!
+  }
+
+  input CredentialsInput {
+    username: String!
+    password: String!
+  }
+
+  input SignUpInput {
+    dni: Int!
+    username: String!
+    password: String!
+    name: String!
+    lastname: String!
+    age: Int!
+    email: String!
+    telephoneNumber: Int!
+    city: String!
+    address: String!
+  }
+
+  type SpecialistDetail {
+    id: Int!
+    dni: Int!
+    name: String!
+    lastname: String!
+    age: Int!
+    email: String!
+    telephone_number: Float!
+    city: String!
+    priceXhour: Int!
+    description: String!
+    category: String!
+    url: String!
+    score: String!
+  }
+
+  type Mutation {
+    signUpUser(userInput: SignUpInput): Tokens!
+    logIn(credentials: CredentialsInput!): Tokens!
+    refreshToken(refresh: String!): Access!
+  }
+
+  type Query {
+    userDetailById(userId: Int!): SpecialistDetail!
+    getAllSpecialist: [SpecialistDetail]!
+    getSpecialistById(specialistId: Int!): SpecialistDetail
+    filterSpecialistByCategory(category: String!): [SpecialistDetail]!
+  }
+`;
+
+module.exports = authTypeDefs;
