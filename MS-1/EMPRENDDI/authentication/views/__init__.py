1885172dfb282ac3e283f0c84b566a78fa1de1d7
Jhonatan Pumarejo <jhonatan.pumarejo.86@gmail.com>
2021-12-03 08:52:39 -0500

    ms1-ep-priceXhour

--- MS-1/EMPRENDDI/authentication/views/__init__.py
+++ MS-1/EMPRENDDI/authentication/views/__init__.py
@@ -3,5 +3,5 @@ from .contractorCreateView import ContractorCreateView
 from .verifyTokenView import VerifyTokenView
 
 from .contractorDetailView import ContractorCreateView2, ContractorGetAllApi, ContractorGetSelectedApi, ContractorDeleteApi
-from .specialistDetailView import SpecialistCreateView, SpecialistGetAllApi, SpecialistGetSelectedApi, SpecialistDeleteApi
+from .specialistDetailView import SpecialistCreateView, SpecialistGetAllApi, SpecialistGetSelectedApi, SpecialistDeleteApi, SpecialistGetSelectedApi_Score, SpecialistGetSelectedApi_priceXhour
 from .specialistDetailByCategory import  SpecialistbyCategory
