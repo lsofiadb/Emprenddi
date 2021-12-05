4b1e00145ee1d79be7e6dd308f10df8089afc2bf
Bakiury <thecutaseta@gmail.com>
2021-12-05 16:25:31 -0500

    Se completan todos los endpoints en el archivo reservation_api.js para el microservicio #2

--- API-GATEWAY/src/dataSources/reservation_api.js
+++ API-GATEWAY/src/dataSources/reservation_api.js
@@ -7,14 +7,42 @@ class ReservationAPI extends RESTDataSource {
     this.baseURL = serverConfig.reservation_api_url;
   }
 
+  async createReserve(reserve) {
+    reserve = new Object(JSON.parse(JSON.stringify(reserve)));
+    return await this.post(`/reservationController/addReservation`, reserve);
+  }
+
   async getAllReservations() {
     return await this.get(`/reservationController/getAllReservations`);
   }
 
   async getReservationsByContractor(contractorId) {
-    return await this.get(
-      `/reservationController/getReservationsByContractor/${contractorId}`
-    );
+    return await this.get(`/reservationController/getReservationsByContractor/${contractorId}`);
+  }
+
+  async deleteReserve(reserveId) {
+    return await this.delete(`/reservationController/deleteReservationById/${reserveId}`);
+  }
+
+  async updateReserve(reserve) {
+    reserve = new Object(JSON.parse(JSON.stringify(reserve)));
+    return await this.put(`/reservationController/updateReservation`, reserve);
+  }
+
+  async getReservesBySpecialist(specialistId) {
+    return await this.get(`/reservationController/getReservationsBySpecialist/${specialistId}`);
+  }
+
+  async getSpecialistScore(specialistId) {
+    return await this.get(`/reservationController/getScoreBySpecialist/${specialistId}`);
+  }
+
+  async getArrayWithReserveTime() {
+    return await this.get(`/reservationController/getTimeByService`);
+  }
+
+  async getReserveById(reserveId) {
+    return await this.get(`/reservationController/getReservationById/${reserveId}`);
   }
 }
 
